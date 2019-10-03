0e8ffdd5d6e101520d44b863f1e030952c32f5d8
Akhil <akhil.chandran@theplayhousegroup.com>
2019-10-03 13:03:44 +0530

    home page final changes

--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,50 @@ jQuery(document).ready(function ($) {
   $('header .menu-right-wrap .search-icon').on('click', function() {
     $('header .menu-right-wrap .search-form-wrap').toggle();
   });
+
+  // video block carousel
+  $('.video-block .video-wrap').slick({
+    infinite: false,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: true,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          infinite: true,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  // Setting height of ad wrap
+
+  setAdWrapHeight();
+  $(window).on('resize', function() {
+    setAdWrapHeight();
+  })
+
+  function setAdWrapHeight() {
+    var slideHeight = $('.video-block .video-wrap .slick-slide').height();
+    $('.video-block .video-wrap .ad-wrap').css('height', slideHeight);
+  }
+
 });
